243195e9d1eb2eb5b8542b019f7a3ad6bf09e868
lemo-wu <lemo-wu@lemo-wudeimac.local>
2018-01-17 19:11:07 +0800

    解决时间友好显示bug

--- WXLKitDemo/main.m
+++ WXLKitDemo/main.m
@@ -8,9 +8,14 @@
 
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
+#import "DateKit.h"
 
 int main(int argc, char * argv[]) {
-    @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+     {
+      
+        //return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+      NSString *result = [DateKit dateDisplayResult:1501553410];
+      NSLog(@"%@", result);
+      return 0;
     }
 }
